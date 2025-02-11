c15c3de22dd9632fd0c7687e3d5b610cb6695ce2
Kishor Shutihar <kix.sharma@gmail.com>
2025-02-11 19:12:55 +0545

    use produce method make changes to state

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,9 +1,57 @@
 import { ListGroup } from "./components/ListGroup";
 import { Button } from "./components/Button";
 import { Like } from "./components/Like";
+import { useState } from "react";
+import { produce } from "immer";
 
 function App() {
   const countries = ["Nepal", "India", "China", "Pakistan"];
+  const [arrOfObj, setArrOfObj] = useState([
+    { name: "Kishor", age: 25, bachelor: true },
+    { name: "Rohit", age: 25, bachelor: false },
+  ]);
+
+  const handleClick = () => {
+    //add
+    // setArrOfObj([...arrOfObj, { name: "samiskhya", age: 30, bachelor: false }]);
+
+    // setArrOfObj(
+    //   produce((draft) => {
+    //     draft.push({ name: "samiskhya", age: 30, bachelor: false });
+    //   })
+    // );
+
+    //update
+    // setArrOfObj(
+    //   arrOfObj.map((arr) => {
+    //     if (arr.name == "Kishor") {
+    //       arr.name = "Kishan";
+    //     }
+    //     return arr;
+    //   })
+    // );
+
+    // setArrOfObj(
+    //   produce((draft) => {
+    //     const findable = draft.find((e) => e.name == "Kishor");
+    //     if (findable) {
+    //       findable.name = "Kishan";
+    //     }
+    //   })
+    // );
+
+    //delete
+    // setArrOfObj(arrOfObj.filter((arr) => arr.name != "Kishor"));
+
+    setArrOfObj(
+      produce((draft) => {
+        const someVal = draft.filter((arr) => arr.name !== "Kishor");
+        return someVal;
+      })
+    );
+  };
+
+  console.log(arrOfObj);
 
   return (
     <>
@@ -12,6 +60,10 @@ function App() {
         heading="Countries"
         onSelectItem={() => console.log("Hello")}
       /> */}
+      <button onClick={handleClick}>Button</button>
+      {arrOfObj.map((value, index) => (
+        <p key={index}>{value.name}</p>
+      ))}
       <Like onClick={() => console.log("Clicked")} />
     </>
   );
